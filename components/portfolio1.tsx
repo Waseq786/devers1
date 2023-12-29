8b78c253b93023a70f519515c055063acd96b1c9
waseq <114839357+Waseq786@users.noreply.github.com>
2023-12-29 13:15:08 +0500

    Add files via upload

---
+++ components/portfolio1.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+const Portfolio = () => {
+    return (
+
+        <div>
+            <div className=' ml-4 lg:ml-4  lg:w-[60%]'>
+
+                <div className=''>
+                    <span className='text-[20px]  lg:text-2xl font-bold'>	Web Design Portfolio: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        Explore our visually striking websites, blending creativity and functionality for a memorable user experience.</p><br />
+
+
+                    <span className='text-[220x]  lg:text-2xl font-bold'>	Web Development Showcase: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        Discover diverse projects showcasing seamless navigation, robust features, and optimal performance.</p><br />
+
+
+                    <span className='text-[20px]  lg:text-2xl font-bold'>	E-Commerce Success Stories: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        See how businesses thrive with our secure and user-friendly online stores, enhancing the customer shopping experience.</p><br />
+
+
+                    <span className='text-[20px]  lg:text-2xl font-bold'>	CMS-Powered Websites: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        Check out websites easily managed with user-friendly Content Management Systems like WordPress or Joomla.</p><br />
+
+
+                    <span className='text-[20px]  lg:text-2xl font-bold'>	Website Maintenance Highlights: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        View examples of websites benefiting from our dedicated maintenance services, ensuring sustained success.</p><br />
+
+
+                    <span className='text-[20px]  lg:text-2xl font-bold'>	Logo Design Gallery: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        Explore distinctive and memorable logos capturing brand essence and resonating with audiences.</p><br />
+
+
+                    <span className='text-[20px]  lg:text-2xl font-bold'>	Responsive Web Design Examples: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        Experience seamless adaptability across devices, ensuring an optimal viewing experience.</p><br />
+
+
+                    <span className='text-[20px]  lg:text-2xl font-bold'>	SEO Successes: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        See improved online visibility and higher search rankings with our tailored SEO strategies.</p><br />
+
+
+                    <span className='text-[20px]  lg:text-2xl font-bold'>	Performance Optimization Showcase: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        Explore websites optimized for speed and efficiency, providing an exceptional user experience.</p><br />
+
+
+                    <span className='text-[20px]  lg:text-2xl font-bold'>Custom Web Solutions Portfolio: </span><br />
+
+                    <p className='text-[15px] mt-[2px] mr-5 text-justify lg:mt-[5px] lg:text-xl'>
+
+                        Delve into our portfolio of custom web solutions catering to unique business needs.</p>
+
+                </div>
+            </div>
+        </div>
+
+
+
+    )
+}
+
+export default Portfolio
